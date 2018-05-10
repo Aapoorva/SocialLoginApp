8e1c98df3ced2c57070e14c77965449ee0bfff43
Apoorva Agarwal <apoorvaagarwal.cse19@jecrc.ac.in>
2018-05-11 05:02:54 +0530

    project at initial stage

---
+++ app/src/main/java/com/example/apoorva/socialloginapp/UtilityClasses/ProjectConstants.java
@@ -0,0 +1,13 @@
+package com.example.apoorva.socialloginapp.UtilityClasses;
+
+/**
+ * Created by Apoorva on 10-May-18.
+ */
+
+public class ProjectConstants {
+    public static String USER_NODE = "users";
+    public static String USER_PROFILE_NODE = "profiledata";
+    public static String USER_PROFILE_IS_SET_NODE = "profileisset";
+    public static String PROFILE_IS_SET_TRUE = "1";
+    public static String PROFILE_IS_SET_FALSE = "0";
+}
